366035d0690ee220a1d0cf17efdf0a8b154462cc
vx007 <vasilyev.x@mail.ru>
2023-01-13 20:54:28 +0300

    Initial commit

---
+++ src/test/java/pro/sky/socksshop/SocksShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.socksshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
