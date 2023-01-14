150b8b1ef65fc187e867b70ee085fc2127edaa88
vx007 <vasilyev.x@mail.ru>
2023-01-14 23:34:09 +0300

    Commit #3

--- src/main/java/pro/sky/socksshop/service/SockService.java
+++ src/main/java/pro/sky/socksshop/service/SockService.java
@@ -3,9 +3,9 @@ package pro.sky.socksshop.service;
 import pro.sky.socksshop.model.Sock;
 
 public interface SockService {
-    int add(Sock sock, Integer quantity);
+    int add(Sock sock, int quantity);
 
-    int remove(Sock sock, Integer quantity);
+    int remove(Sock sock, int quantity);
 
     int getMin(Sock sock);
 
